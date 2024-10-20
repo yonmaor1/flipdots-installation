dc5f8def407f45748eb04ef272bf4679d152d5b0
yonmaor1 <yonmaor@cmu.edu>
2024-10-19 22:07:27 -0700

    removed package.json, created golive.js

---
+++ golive.js
@@ -0,0 +1,23 @@
+const { exec } = require('child_process');
+
+// Start the Python server
+const python_server = exec('python3 -m http.server 8000', { cwd: __dirname });
+
+python_server.stdout.on('data', (data) => {
+  console.log(`Python Server: ${data}`);
+});
+
+python_server.stderr.on('data', (data) => {
+  console.error(`Python Server Error: ${data}`);
+});
+
+// Start the Node.js server
+const nodeServer = exec('node server.js', { cwd: __dirname });
+
+nodeServer.stdout.on('data', (data) => {
+  console.log(`Node.js Server: ${data}`);
+});
+
+nodeServer.stderr.on('data', (data) => {
+  console.error(`Node.js Server Error: ${data}`);
+});
