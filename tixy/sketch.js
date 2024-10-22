553668a1f3e828488cb16859e78a9d12052a9d71
yonmaor1 <yonmaor@cmu.edu>
2024-10-21 20:10:27 -0400

    added P as input to expression

--- tixy/sketch.js
+++ tixy/sketch.js
@@ -4,9 +4,11 @@ let function_field;
 let canvas;
 
 let f_str = 'sin(t-sqrt((x-7.5)**2+(y-6)**2))';
-let color_func = eval(`(x,y,i,t) => ${f_str}`);
+let color_func = eval(`(x,y,i,t,P) => ${f_str}`);
 
 let frames_since = 0;
+
+let pixs = []
 
 function setup() {
     canvas = createCanvas(800, 600);
@@ -18,9 +20,15 @@ function setup() {
     function_field.style('font-size', '16px'); // Adjust the font size here
     function_field.value(f_str);
     windowResized();
+
+    for (let i = 0; i < NUM_COLS * NUM_ROWS; i++) {
+        pixs.push(0)
+    }
 }
 
 function is_valid_function(f_str) {
+
+    print(f_str);
     
     if (!f_str) {
         return false;
@@ -28,10 +36,11 @@ function is_valid_function(f_str) {
 
     try {
         // try to create a new function with the provided string
-        let test_f = new Function(`return (x, y, i, t) => ${f_str}`);
-        test_f()(0, 0, 0, 0);
+        let test_f = new Function(`return (x, y, i, t, P) => ${f_str}`);
+        test_f()(0, 0, 0, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0]);
         return true;
     } catch (e) {
+        print(e)
         return false;
     }
 }
@@ -45,19 +54,26 @@ function func_input() {
     }
     if (is_valid_function(f_str)) {
         try {
-            color_func = eval(`(x, y, i, t) => ${f_str}`);
+            color_func = eval(`(x, y, i, t, P) => ${f_str}`);
             frames_since = 0;
+            for (let i = 0; i < NUM_COLS * NUM_ROWS; i++) {
+                pixs[i] = 0
+            }
         } catch (e) { // probably in the middle of typing
             try {
-                color_func = eval(`(x, y, i, t) => ${prev_f_str}`);
+                color_func = eval(`(x, y, i, t, P) => ${prev_f_str}`);
                 f_str = prev_f_str;
+                for (let i = 0; i < NUM_COLS * NUM_ROWS; i++) {
+                    pixs[i] = 0
+                }
             } catch (e) { // something went wrong
                 color_func = () => 0; // fallback to a default function
                 f_str = '0'
             }
         }
     } else {
-        color_func = eval(`(x, y, i, t) => ${prev_f_str}`);
+        print("failed again")
+        color_func = eval(`(x, y, i, t, P) => ${prev_f_str}`);
         f_str = prev_f_str;
     }
 
@@ -90,8 +106,30 @@ function draw_info() {
     fill('white');
     textSize(16);
     let displayStr = "// input must be valid javascript expression\n";
-    displayStr += "(x,y,i,t) =>";
+    displayStr += "(x,y,i,t,P) =>";
     text(displayStr, 20, height - 150);
+}
+
+function get_prev_bits(index) {
+
+    let prev_bits = [];
+
+    let x = index % NUM_COLS;
+    let y = Math.floor(index / NUM_COLS);
+    for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+            let prev_bit_x = x + i;
+            let prev_bit_y = y + j;
+
+            if (prev_bit_x < 0 || prev_bit_x >= NUM_COLS || prev_bit_y < 0 || prev_bit_y >= NUM_ROWS) {
+                prev_bits.push(0);
+            } else {
+                prev_bits.push(pixs[(x + i) + (y + j) * NUM_COLS]);
+            }
+        }
+    }
+
+    return prev_bits;
 }
 
 function draw_tixy_grid(f) {
@@ -103,7 +141,10 @@ function draw_tixy_grid(f) {
         for (let y = 0; y < NUM_ROWS; y++) {
             let i = x + y * NUM_COLS;
             let t = frames_since;
-            let bit = draw_tixy_cell(x, y, i, t, f);
+            let P = get_prev_bits(i);
+            
+            let bit = draw_tixy_cell(x, y, i, t, P, f);
+            pixs[i] = bit;
 
             // if (y == 0) {
             //     panel_1_bits.push(0);
@@ -120,7 +161,7 @@ function draw_tixy_grid(f) {
     }
 }
 
-function draw_tixy_cell(x, y, i, t, f) {
+function draw_tixy_cell(x, y, i, t, P, f) {
     let margin = 30;
     let gridAreaWidth = (width - 2 * margin);
     let cellSize = gridAreaWidth / NUM_COLS;
@@ -128,12 +169,11 @@ function draw_tixy_cell(x, y, i, t, f) {
     let px = x * cellSize + (cellSize / 2) + margin;
     let py = y * cellSize + (cellSize / 2) + margin;
 
-    let v = (f(x, y, i, t) > 0) ? 1 : 0;
+    let v = (f(x, y, i, t, P) > 0) ? 1 : 0;
     let c = v ? 'white' : 'black';
-    let s = v ? 'black' : 'white';
 
     fill(c);
-    noStroke(); //stroke(128);
+    noStroke();
     circle(px, py, 0.9 * cellSize);
     return v;
 }
